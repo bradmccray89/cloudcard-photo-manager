b64a88b4a2fe067123a3128df0efdcd403a80e3e
Brandon McCray <bradmccray89@gmail.com>
2020-12-30 22:30:18 -0500

    file selection finally working

--- src/components/FileStorage.vue
+++ src/components/FileStorage.vue
@@ -1,22 +1,38 @@
 <template>
     <v-container>
         <div id="folder-input">
-            <input id="folder-input-button" type="file" webkitdirectory directory />
+            <v-btn class="ma-2" @click="activateFolderInput()">Select Folder</v-btn>
+            <input id="folder-input-button" @change="saveFolderSelection" type="file" webkitdirectory directory />
         </div>
     </v-container>
 </template>
 
 <script>
+    const path = require('path')
+    const removeFileName = dirname => path.parse(dirname).dir
+
     export default {
         name: 'FileStorage',
 
         props: {
         },
 
-        created: function () {
+        data: {
+            folderInput: '',
+            selectedPath: ''
         },
 
         methods: {
+            activateFolderInput() {
+                this.folderInput = document.getElementById('folder-input-button')
+                this.folderInput.click()
+            },
+            saveFolderSelection(event) {
+                this.selectedPath = event.path[0].files[0].path
+                if (this.selectedPath) {
+                    this.selectedPath = removeFileName(this.selectedPath)
+                }
+            }
         }
     }
 </script>
@@ -26,6 +42,10 @@
         border: 1px dashed rgba(0, 0, 0, 0.3);
     }
     #folder-input-button {
+        display: none;
         margin: 8px;
+    }
+    .container {
+        padding: 12px 0;
     }
 </style>
